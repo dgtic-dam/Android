db1ae10c467e988a4216f7f7c25ed6d1cb4c23b3
Dannas_Cornell <dannas@protonmail.com>
2019-09-06 20:54:28 -0500

    RecyclerView was updated

--- androidbasico2019/RecyclerViewExample/app/src/main/java/com/cybertch/recyclerviewexample/adapter/ContactAdapter.kt
+++ androidbasico2019/RecyclerViewExample/app/src/main/java/com/cybertch/recyclerviewexample/adapter/ContactAdapter.kt
@@ -22,8 +22,20 @@ import kotlinx.android.synthetic.main.item_contact.view.*
  * @since:
  *
  **/
-class ContactAdapter(val contacts:ArrayList<Contact>,val context: Context){
+class ContactAdapter(val contacts:ArrayList<Contact>,val context: Context):
+    RecyclerView.Adapter<ContactViewHolder>(){
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ContactViewHolder {
+        return ContactViewHolder(LayoutInflater.from(context).inflate(R.layout.item_contact,p0, false))
+    }
 
+    override fun getItemCount(): Int {
+        return contacts.size
+    }
+
+    override fun onBindViewHolder(p0: ContactViewHolder, p1: Int) {
+        p0?.nameTextView?.text=contacts.get(p1).name
+        p0?.lastNameTextView?.text=contacts.get(p1).lastname
+    }
 }
 
 class ContactViewHolder(view: View):RecyclerView.ViewHolder(view){
